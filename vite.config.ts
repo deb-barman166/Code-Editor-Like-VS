2d70b6c8c3902a43ba83783a79b9de1d4fa08685
Deb Barman <barmandeb166@gmail.com>
2026-03-24 12:26:42 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 import {VitePWA} from 'vite-plugin-pwa';
 
+export default defineConfig({
+  base: "/Code-Editor-Like-VS/",
+})
+
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
