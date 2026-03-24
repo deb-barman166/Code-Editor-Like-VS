04f06c9684cc699c59c9b818b296c6fe429c231e
Deb Barman <barmandeb166@gmail.com>
2026-03-24 12:42:31 +0530

    Remove tailwindcss plugin from vite.config.ts
    Removed tailwindcss plugin from Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
@@ -10,7 +9,6 @@ export default defineConfig(({mode}) => {
     base: "/Code-Editor-Like-VS/",
     plugins: [
       react(),
-      tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
         includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
